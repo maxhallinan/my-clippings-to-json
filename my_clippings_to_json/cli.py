e42817cefb4197b2595dfe428ff79861e6f95262
Maximum Hallinan <maxhallinan@gmail.com>
2016-06-10 13:43:26 -0400

    Process input file

--- my_clippings_to_json/cli.py
+++ my_clippings_to_json/cli.py
@@ -1,14 +1,15 @@
 import click
+import clippings
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument(
-    'input',
+    'input_path',
     required=True,
     type=click.Path(exists=True))
 @click.argument(
-    'output',
+    'output_path',
     required=True)
 @click.option(
     '--start',
@@ -16,5 +17,8 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
     default=1,
     type=int,
     help='start at line number of input file')
-def main(input, output, start):
-    click.echo('Hello World!')
+def main(input_path, output_path, start):
+    last_line_number = clippings.main(
+        input_path, 
+        output_path, 
+        start_line=start)
