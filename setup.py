8e314490cc0e869f6df35f96b0ed52f9c6970c53
Maximum Hallinan <maxhallinan@gmail.com>
2016-06-12 12:00:54 -0400

    Configure setup

--- setup.py
+++ setup.py
@@ -3,18 +3,29 @@ from setuptools import find_packages, setup
 dependencies = ['click']
 
 setup(
-    name='my_clippings_to_json',
-    version='0.1.0',
-    url='https://github.com/maximumhallinan/my-clippings-to-json',
     author='Maximum Hallinan',
-    description='A command-line tool for converting \'My Clippings.txt\' to JSON',
-    platforms='any',
-    packages=find_packages(exclude=['tests']),
-    include_package_data=True,
-    install_requires=dependencies,
+    author_email='maxhallinan@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    description='Format \'Kindle/documents/My Clippings.txt\' as JSON',
     entry_points={
         'console_scripts': [
             'my_clippings_to_json = my_clippings_to_json.cli:main',
         ],
-    }        
-)
+    },
+    include_package_data=True,
+    install_requires=dependencies,
+    keywords='Kindle My Clippings highlights notes bookmarks',
+    license='MIT',
+    name='my_clippings_to_json',
+    packages=find_packages(exclude=['tests']),
+    platforms='any',
+    url='https://github.com/maximumhallinan/my-clippings-to-json',
+    version='0.1.0',
+    zip_safe=False)
